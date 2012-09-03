dcbaae6a456eddef147cadd61bd82d11cef6c646
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2012-09-03 07:58:42 -0500

    Normalized naming in tube emptier script
    empty-tube.py looked like it was written very quickly
    by a tired coder who needed something that worked RIGHT NOW. Because that's
    exactly how it happened. It is now named like everything else and
    obeys DRY.

---
+++ masterblaster/empty_tube.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+### Missouri S&T ACM SIG-Game Arena (Thunderdome)
+#####
+
+from config import game_name
+
+tube = 'game-requests-' + game_name
+
+import beanstalkc
+c = beanstalkc.Connection()
+c.watch(tube)
+while c.stats_tube(tube)['current-jobs-ready'] > 0:
+  j = c.reserve()
+  j.delete()
