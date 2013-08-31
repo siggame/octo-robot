4ad15ce14543720ade1174870f2ac2699ddd3dad
Brandon Phelps <bwp44f@mst.edu>
2013-08-30 19:02:34 -0500

    updated overlord

--- gladiator/overlord.py
+++ gladiator/overlord.py
@@ -7,8 +7,8 @@ import re
 from subprocess import Popen, PIPE
 
 server = Popen(['python', 'main.py', '-arena'], stdout=PIPE, stderr=PIPE, cwd="server")
-refereeOne = Popen(['python', 'refer.py', os.environ['SERVER_PATH']], stdout=PIPE,
-                   stderr=PIPE, stdin=PIPE, cwd='1')
+#refereeOne = Popen(['python', 'refer.py', os.environ['SERVER_PATH']], stdout=PIPE,
+#                   stderr=PIPE, stdin=PIPE, cwd='1')
 
 pattern = re.compile('Game [0-9]+ over')
 
@@ -17,5 +17,6 @@ while True:
         output = server.stdout.readline()
 
         if output != '':
-            result = pattern.match(output)
+            result = pattern.search(output)
             print(result)
+            break
