28592b8353c1f89575f9888e5bb68089df824e9c
brandon <bwp44f@mst.edu>
2013-09-03 17:00:04 -0500

    adding fake referee, scheduler, modified fake archiver, this will be removing the need for clients. Thus making it possible to test some of the arena functionalities without clientsor test ais

---
+++ gladiator/fake_ref.py
@@ -0,0 +1,95 @@
+
+
+import re
+import json
+import subprocess
+import os
+import signal
+import random
+import socket
+import md5
+import zipfile
+from time import sleep
+from datetime import datetime
+from bz2 import BZ2File
+
+
+def main():
+    stalk = beanstalkc.Connection(host=os.environ['BEANSTALK_HOST'])
+    stalk.watch('game-requests-%s' % os.environ['GAME_NAME']) # input
+    stalk.use('game-results-%s' % os.environ['GAME_NAME']) # output
+    
+    while True:
+        looping(stalk)
+        
+def looping(stalk):
+    
+    job = stalk.reserve()
+    game = json.loads(job.body)
+    
+    print "processing game", game['number']
+    
+    game['blaster_id'] = socket.gethostname()
+    game['referee_id'] = os.getpid()
+    game['started'] = str(datetime.now())
+    
+    if 'tournament' not in game:
+        for client in game['clients']:
+            update_local_repo(client)
+    
+    for prefix in [x['name'] for x in game['clients']]:
+        for suffix in ['stdout', 'stderr', 'makeout', 'gitout']:
+            with file('%s-%s.txt' % (prefix, suffix), 'w') as f:
+                f.write('empty')
+                
+    game['status'] = "Building"
+    stalk.put(json.dumps(game))
+    job.touch()
+    
+    for client in game['clients']:
+        client['compiled'] = (compile_client(client) is 0)
+        job.touch()
+        print "result for make in %s was %s" % (client['name'],
+                                                client['compiled'])
+    
+    if not all(x['compiled'] for x in game['clients']]):
+        print "failing the game, someone didn't compile"
+        game['status'] = "Failed"
+        game['completed'] = str(datetime.now())
+        #push_datablocks(game)
+        stalk.put(json.dumps(game))
+        job.delete()
+        return
+    
+    
+    print "running...", game['number']
+    game['status'] = "Running"
+    stalk.put(json.dumps(game))
+    sleep(11)
+    
+    
+    print "determining winner..."
+    winner = random.randint(0, 2)
+    if winner = 2:
+        game['tied'] = True
+        print game['clients'][0]['name'], "and", \
+            game['clients'][1]['name'], "tied!"
+    else:
+        if winner == 0:
+            game['winner'] = game['clients'][0]
+            game['loser'] = game['clients'][1]
+        elif winner == 1:
+            game['winner'] = game['clients'][1]
+            game['loser'] = game['clients'][1]
+        print game['winner']['name'], "beat", game['loser']['name']
+        
+    # clean up
+    game['status'] = "Complete"
+    game['completed'] = str(datetime.now())
+    stalk.put(json.dumps(game))
+    job.delete()
+    print "%s done %s" % (game['number'], str(datetime.now())
+    
+    
+main()
+                               
