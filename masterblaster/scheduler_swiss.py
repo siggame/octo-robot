712ab508f64121c137c36756bbbddd6ea13c7bec
Ubuntu <ubuntu@domU-12-31-39-0E-7A-B4.compute-1.internal>
2013-11-08 18:03:36 +0000

    updated scheduler_swiss, still work in progress

--- masterblaster/scheduler_swiss.py
+++ masterblaster/scheduler_swiss.py
@@ -37,35 +37,36 @@ from thunderdome.config import game_name
 from thunderdome.models import Client , Game
 from thunderdome.sked import sked
 
-round = 1
+rounds = 1
 match = 1
 
+
 def main():
-    global round, match
-    #stalk = beanstalkc.Connection()
+    global rounds, match
+    stalk = beanstalkc.Connection()
     req_tube = "game-requests-%s" % game_name
+    stalk.use(req_tube)
     stop = False
-    client_dict = defaultdict(int) #key is client name, value is score
+    client_dict = defaultdict(int)  #key is client name, value is score
     while not stop:
         try:
-            #open the stalk
-            stalk = beanstalkc.Connection()
-            stalk.use(req_tube)
             stats = stalk.stats_tube(req_tube)
             #check if we need to schedual another round
             if stats['current-jobs-ready'] <= 0:
                 update_clients()
                 #schedule the next round
+                print "Scheduling a Round"
                 schedule_a_round(stalk, client_dict)
-                round += 1
-            stalk.close()
+                print "Scheduling round over"
+                rounds += 1
         except:
             print "Arena scheduler could not schedule a game"
-        time.sleep(.1)
+        time.sleep(1)
+    stalk.close()
 
 
 def schedule_a_round(stalk, scores):
-    global round, match
+    global rounds, match
     '''Schedule the games for the next round'''
     clients = list(Client.objects.exclude(name='bye').filter(embargoed=False))
     #make sure each client has a entry in scores
@@ -73,39 +74,47 @@ def schedule_a_round(stalk, scores):
     for player in clients:
         if player.name not in scores.keys():
             scores[player.name] = 0
-       
+
+    print "Scores has been built"
+
     #check if new match needs to be played
     for rank in scores.values():
-        if rank[1] > (math.log(2, len(clients))/2+2):
+        if rank[1] > (math.log(2, len(clients)) / 2 + 2):
             match += 1
-            round = 1 
+            rounds = 1
             # should clear out clients, and their scores?
             return
-    
+
+    print "Updating Scores"
     #search old games for all games in round -1
+    origin_msg = "Arena Scheduler-Swiss: Match %d Round %d" % (match, rounds - 1)
     games = list(Game.objects.all())
     gamesLastRound = []
     for currentGame in games:
         #unpack stats
         stats = json.loads(currentGame.stats)
         #seach stats for all games last round
-        if stats['origin'] is "Arena Scheduler-Swiss: Match " + match + " Round "+round - 1:
+        if stats['origin'] is origin_msg:
             gamesLastRound.append(currentGame)
+
     #modify client's score by who won last round, no clear win, no points
     for gamePlayed in gamesLastRound:
         if gamePlayed.winner.name in scores.keys():
-            scores[gamePlayed.winner.name] = scores[gamePlayed.winner.name]+1
+            scores[gamePlayed.winner.name] = scores[gamePlayed.winner.name] + 1
+
+    print "Scores have been updated"
     #sort clients by score
-    sorted_scores = sorted([(name, score) for name, score in scores.iteritems], 
-                            key = lambda x: x[1], reverse=True)
-    
+    sorted_scores = sorted([(name, score) for name, score in scores.iteritems],
+                           key = lambda x: x[1], reverse=True)
+
     #loop through and have top to play each other
-    origin = "Arena Scheduler-Swiss: Match " + match + " Round " + round
+    origin = "Arena Scheduler-Swiss: Match " + match + " Round " + rounds
     while len(sorted_scores) >= 2:
         p1 = sorted_scores.pop(0)
         p2 = sorted_scores.pop(0)
-        sked(Client.objects.get(name=p1[0]), Client.objects.get(name=p2[0]), stalk, origin)
-        
+        sked(Client.objects.get(name=p1[0]),
+             Client.objects.get(name=p2[0]), stalk, origin)
+
     #last guy auto wins
     if sorted_scores:
         p0 = sorted_scores.pop()
