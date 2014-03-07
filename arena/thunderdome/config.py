16663eee8846e0136d891d662b682311ee093890
Brandon Phelps <bwp44f@mst.edu>
2014-03-07 15:46:37 -0600

    thunderdome config

--- arena/thunderdome/config.py
+++ arena/thunderdome/config.py
@@ -7,12 +7,12 @@
 
 from arena.settings.aws_secrets import access_cred, secret_cred, s3_prefix
 from arena.settings.secret_settings import WEBSITE_USER_NAME, WEBSITE_ARENA_PASSWORD
-        
-game_name = ''
-beanstalk_host = ''
-client_prefix = ''
+
+game_name = 'chess-2014'
+beanstalk_host = 'r99acm.device.mst.edu'
+client_prefix = 'ssh://webserver@megaminerai.com'
 arena_ami = ''
 tournament_ami = ''
 req_queue_len = 5
 api_url_template = "http://megaminerai.com/api/repo/tags/%s/"
-arena_head = ''
+arena_head = 'r99acm.device.mst.edu'
