deb1edf6ecf916940f67e3332a6af1f3bdab5829
Brandon Phelps <bwp44f@mst.edu>
2014-04-13 04:28:50 +0000

    update prep

--- gladiator/prep_for_bake.py
+++ gladiator/prep_for_bake.py
@@ -11,7 +11,6 @@ import requests
 
 game_name = os.environ["GAME_NAME"]
 
-
 def main():
     api_url = "http://megaminerai.com/api/repo/tags/%s" % game_name
     r = requests.get(api_url, auth=(WEB_USER, WEB_PASS))
@@ -26,28 +25,28 @@ def main():
     #f.close()
     
     for block in data:
-        if block['team']['name'] is not None:
+        if block['team']['slug'] is not None:
             update_local_repo(block)
 
 def update_local_repo(client):
     '''Get the appropriate code and version from the repository'''
     base_path = os.environ['CLIENT_PREFIX']
-    subprocess.call(['rm', '-rf', '%s' % client['team']['name']],
+    subprocess.call(['rm', '-rf', '%s' % client['team']['slug']],
                     stdout=file('/dev/null'),
                     stderr=subprocess.STDOUT)
     subprocess.call(['git', 'clone',
-                     '%s%s' % (base_path, client['repository']['path']), client['team']['name']],
-                    stdout=file('%s-gitout.txt' % client['team']['name'], 'w'),
+                     '%s%s' % (base_path, client['repository']['path']), client['team']['slug']],
+                    stdout=file('%s-gitout.txt' % client['team']['slug'], 'w'),
                     stderr=subprocess.STDOUT)
-    subprocess.call(['git', 'checkout', 'master'], cwd=client['team']['name'],
-                    stdout=file('%s-gitout.txt' % client['team']['name'], 'a'),
+    subprocess.call(['git', 'checkout', 'master'], cwd=client['team']['slug'],
+                    stdout=file('%s-gitout.txt' % client['team']['slug'], 'a'),
                     stderr=subprocess.STDOUT)
-    subprocess.call(['git', 'pull'], cwd=client['team']['name'],
-                    stdout=file('%s-gitout.txt' % client['team']['name'], 'a'),
+    subprocess.call(['git', 'pull'], cwd=client['team']['slug'],
+                    stdout=file('%s-gitout.txt' % client['team']['slug'], 'a'),
                     stderr=subprocess.STDOUT)
-    subprocess.call(['git', 'checkout', client['tag']['name']],
-                    stdout=file('%s-gitout.txt' % client['team']['name'], 'a'),
+    subprocess.call(['git', 'checkout', client['tag']['slug']],
+                    stdout=file('%s-gitout.txt' % client['team']['slug'], 'a'),
                     stderr=subprocess.STDOUT,
-                    cwd=client['team']['name'])
+                    cwd=client['team']['slug'])
 
 main()
