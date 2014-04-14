71d636f6bc0dacb365f0a810b30f7ffced83bc47
brandon phelps <bwp44f@mst.edu>
2014-04-14 11:28:21 -0500

    set default values in thunderdome config

--- arena/thunderdome/config.py
+++ arena/thunderdome/config.py
@@ -8,10 +8,10 @@
 from arena.settings.aws_secrets import access_cred, secret_cred, s3_prefix
 from arena.settings.secret_settings import WEBSITE_USER_NAME, WEBSITE_ARENA_PASSWORD
 
-game_name = 'megaminerai-13-droids'
-beanstalk_host = 'arena.mnuck.com'
+game_name = ''
+beanstalk_host = ''
 client_prefix = 'ssh://webserver@megaminerai.com'
-arena_ami = 'ami-d9adbeb0'
+arena_ami = ''
 tournament_ami = ''
 req_queue_len = 5
 api_url_template = "http://megaminerai.com/api/repo/tags/%s/"
