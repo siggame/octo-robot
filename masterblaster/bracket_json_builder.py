50cb03088169de4f3ef4c0170dc51d0001539e05
Brandon Phelps <bwp44f@mst.edu>
2014-10-09 20:02:02 -0500

    set up bracket json outputer, is validated by jsonshcema

--- masterblaster/bracket_json_builder.py
+++ masterblaster/bracket_json_builder.py
@@ -1,9 +1,38 @@
-
 from thunderdome.models import Match
 from pprint import pprint
+from copy import deepcopy
 import random
+import jsonschema
+import json
 
 loop = 0
+
+schema = {
+    "description" : "Match result",
+    "id" : "match",
+    "type" : "object",
+    "required" : ["winner", "log_location", "previous_matches"],
+    "properties" : {
+        "match_id" : { 
+            "type" : "string",
+        },
+        "winner" : {
+            "type" : "string",
+        },
+        "log_location" : {
+            "type" : "string",
+        },
+        "previous_matches" : {
+            "type" : "array",
+            "items" : {
+                "type" : "object",
+                "minItems" : 2,
+                "maxItems" : 2,
+                "$ref" : "match",
+            }
+        }
+    }
+}
 
 def main():
     tournament_id = 20133422
@@ -11,21 +40,18 @@ def main():
 
     k = {}
     update_brackets(k, championship, 0)
-    
-    pprint(k)
-    #pprint(id_array)
+    jsonschema.validate(k, schema)
+    print json.dumps(k, indent=4)
 
 def get_best_gamelog(match):
     games = list(match.games.all())
     if games:
         return random.choice(games).gamelog_url
     else:
-        return ''
+        return "none"
 
 
 id_array = {}
-
-from copy import deepcopy
 
 def update_brackets(parent_node, match, depth):
     global loop
@@ -33,26 +59,30 @@ def update_brackets(parent_node, match, depth):
     father_dict = {}
     mother_dict = {}
     my_dict = {}
-    my_dict.update({match.pk : {'gamelog' : get_best_gamelog(match),
-                                'prev_games' : [father_dict, mother_dict]}})
+    my_dict.update({"winner" : match.winner.name, 
+                    "match_id" : str(match.pk),
+                    "log_location" : get_best_gamelog(match),
+                    "previous_matches" : []})
+
     loop += 1
     parent_node.update(my_dict)
-    print loop, depth, match.pk
 
     id_array.update({match.pk : my_dict})
     
     if match.father and match.father.pk in id_array.keys():
-        my_dict[match.pk]['prev_games'][0] = id_array[match.father.pk]
+        my_dict["previous_matches"].append(id_array[match.father.pk])
         return
 
     if match.father and match.father.pk not in id_array.keys():
+        my_dict["previous_matches"].append(father_dict)
         update_brackets(father_dict, match.father, depth+1)
     
     if match.mother and match.mother.pk in id_array.keys():
-        my_dict[match.pk]['prev_games'][1] = id_array[match.mother.pk]
+        my_dict["previous_matches"].append(id_array[match.mother.pk])
         return
 
     if match.mother and match.mother.pk not in id_array.keys():
+        my_dict["previous_matches"].append(mother_dict)
         update_brackets(mother_dict, match.mother, depth+1)
 
 if __name__ == "__main__":
