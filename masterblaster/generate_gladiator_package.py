273a86f7a85d91cb9998f70e9d3c977b2b6e600e
Brandon Phelps <bwp44f@mst.edu>
2014-10-25 22:19:55 -0500

    cleaned up generate gladiator package

--- masterblaster/generate_gladiator_package.py
+++ masterblaster/generate_gladiator_package.py
@@ -2,38 +2,53 @@ from thunderdome.config import game_name
 import subprocess
 import shutil
 import os
-
-file_path = os.path.abspath(__file__)
-home_dir = os.path.dirname(file_path)
-octo_robot_dir = os.path.dirname(home_dir)
+import sys
 
 
-gitrepo = 'git@github.com:siggame/MegaMinerAI-13'
-repo_dir = os.path.join(octo_robot_dir, game_name)
-server_location = os.path.join(repo_dir, 'server')
-gladiator_location = os.path.join(octo_robot_dir, 'gladiator')
+def main():
+    print sys.argv
+    if len(sys.argv) < 2:
+        print "Requires location of megmainer ai repo"
+        print "Also ensure that this machine and ssh clone access to the repo"
+        return
 
-if not os.path.exists(repo_dir):
-    subprocess.call(['git', 'clone', gitrepo, game_name], cwd=octo_robot_dir)
-else:
-    subprocess.call(['git', 'pull'], cwd=repo_dir)
+    file_path = os.path.abspath(__file__)
+    home_dir = os.path.dirname(file_path)
+    octo_robot_dir = os.path.dirname(home_dir)
 
-gladiator_p_folder = os.path.join(octo_robot_dir, 'gladiator_package')
-gladiator_p_server = os.path.join(gladiator_p_folder, 'server')
+    gitrepo = 'git@github.com:siggame/%s' % sys.argv[1]
+    repo_dir = os.path.join(octo_robot_dir, game_name)
+    server_location = os.path.join(repo_dir, 'server')
+    gladiator_location = os.path.join(octo_robot_dir, 'gladiator')
+    
+    if not os.path.exists(repo_dir):
+        git_clone_id = subprocess.call(['git', 'clone', gitrepo, game_name], cwd=octo_robot_dir)
+        if git_clone_id != 0:
+            print "error on clone exiting"
+            return
+    else:
+        subprocess.call(['git', 'pull'], cwd=repo_dir)
 
-gladiator_files = os.listdir(gladiator_location)
+        
 
-for i in gladiator_files:
-    print i
+    gladiator_p_folder = os.path.join(octo_robot_dir, 'gladiator_package')
+    gladiator_p_server = os.path.join(gladiator_p_folder, 'server')
+        
+    gladiator_files = os.listdir(gladiator_location)
 
-if os.path.exists(gladiator_p_folder):
-    shutil.rmtree(gladiator_p_folder)
+    if os.path.exists(gladiator_p_folder):
+        shutil.rmtree(gladiator_p_folder)
 
-os.makedirs(gladiator_p_folder)
-shutil.copytree(server_location, gladiator_p_server)
-for i in gladiator_files:
-    shutil.copy(os.path.join(gladiator_location, i), os.path.join(gladiator_p_folder, i))
+    os.makedirs(gladiator_p_folder)
+    shutil.copytree(server_location, gladiator_p_server)
+    for i in gladiator_files:
+        shutil.copy(os.path.join(gladiator_location, i), os.path.join(gladiator_p_folder, i))
 
+    # shutil.make_archive(gladiator_p_folder, 'gztar', octo_robot_dir, gladiator_p_folder)
 
-shutil.make_archive(gladiator_p_folder, 'gztar', octo_robot_dir, gladiator_p_folder)
+    # print gladiator_p_folder
+    # subprocess.call(['tar', 'cvf', 'gladiator_package.tgz', '*'], cwd=gladiator_p_folder)
+    # subprocess.call(['mv', 'gladiator_package.tgz', '../.'], cwd=gladiator_p_folder)
 
+if __name__ == "__main__":
+    main()
