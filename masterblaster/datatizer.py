3b31ef1d6fa43aed14247df3d0109701b2fa8616
Brandon Phelps <bwp44f@mst.edu>
2015-03-07 10:47:11 -0600

    added datatizer file

---
+++ masterblaster/datatizer.py
@@ -0,0 +1,12 @@
+from k_storage.models import add_data_point
+from thunderdome.models import Game
+
+
+def update_all_data():
+    pass
+
+def update_all_():
+    pass
+
+def add_gamelog_data(gameObject):
+    pass
