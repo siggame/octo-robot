8d62d251ee6761e5ff744ca6c54fcea2e6ff4b64
Brandon Phelps <bwp44f@mst.edu>
2015-03-19 21:00:48 -0500

    update glogdl to download all the games and put them into the folders

--- masterblaster/utilities/glogdl.py
+++ masterblaster/utilities/glogdl.py
@@ -2,6 +2,8 @@ import urllib
 import os
 import sys
 
+from thunderdome.models import Game
+
 def download_glog(url, download_destination='.'):
     absolute_path = os.path.abspath(os.path.join('.', download_destination))
     print os.path.exists(absolute_path)
@@ -19,8 +21,16 @@ def glog_name(url):
     return t[len(t)-1]
 
 
+def download_all_gamelogs(download_folder='var/static/gladiator'):
+    for i in list(Game.objects.all()):
+        if i.gamelog_url:
+            client_folder1 = os.path.join(download_folder, list(i.clients.all())[0])
+            client_folder2 = os.path.join(download_folder, list(i.clients.all())[1])
+            download_glog(i.gamelog_url, client_folder1)
+            download_glog(i.gamelog_url, client_folder2)
+
 def main():
-    download_glog("http://siggame-arena-test-bucket.s3.amazonaws.com/logs/megaminerai-15-pharaoh/859-02771.glog", 'var/static/gladiator')
+    # download_glog("http://siggame-arena-test-bucket.s3.amazonaws.com/logs/megaminerai-15-pharaoh/859-02771.glog", 'var/static/gladiator')
 
 
 if __name__ == "__main__":
