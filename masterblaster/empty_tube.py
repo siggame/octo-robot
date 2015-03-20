736b61accede8c4cd795f99673159f4faa168c65
Brandon Phelps <bwp44f@mst.edu>
2015-03-19 20:14:44 -0500

    cleaned up empty_tube script

--- masterblaster/empty_tube.py
+++ masterblaster/empty_tube.py
@@ -1,11 +1,11 @@
 
 from thunderdome.config import game_name
-
-tube = 'game-requests-' + game_name
-
 import beanstalkc
-c = beanstalkc.Connection()
-c.watch(tube)
-while c.stats_tube(tube)['current-jobs-ready'] > 0:
-    j = c.reserve()
-    j.delete()
+
+if __name__ == "__main__":
+    tube = 'game-requests-' + game_name
+    c = beanstalkc.Connection()
+    c.watch(tube)
+    while c.stats_tube(tube)['current-jobs-ready'] > 0:
+        j = c.reserve()
+        j.delete()
