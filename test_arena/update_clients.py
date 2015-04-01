47a738ec9e50cf555d360a84a3c91d21d4c0f757
Brandon Phelps <bwp44f@mst.edu>
2015-04-01 13:50:20 -0500

    obtain repo name

--- test_arena/update_clients.py
+++ test_arena/update_clients.py
@@ -3,7 +3,9 @@ from test_arena.username_web_parser import get_all_forks
 from thunderdome.config import game_name
 
 def update_clients_from_github():
-    print game_name
+    subs = game_name.split("-")
+    repo_name = game_name.split("-")[::-1][0]
+    print repo_name
 
 
 if __name__ == "__main__":
