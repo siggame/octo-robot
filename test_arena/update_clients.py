88387b1c344e782fd605eaf27b67dea631ce4b31
Brandon Phelps <bwp44f@mst.edu>
2015-04-01 13:46:53 -0500

    added update_clients to test_arena

---
+++ test_arena/update_clients.py
@@ -0,0 +1,10 @@
+from thunderdome.models import Client
+from test_arena.username_web_parser import get_all_forks
+from thunderdome.config import game_name
+
+def update_clients_from_github():
+    print game_name
+
+
+if __name__ == "__main__":
+    update_clients_from_github()
