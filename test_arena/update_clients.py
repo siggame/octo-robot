87105f5b95fab1a248dccf5aa0274e07c6456085
Brandon Phelps <bwp44f@mst.edu>
2015-04-01 13:52:48 -0500

    use get forks to get urls of all the clients

--- test_arena/update_clients.py
+++ test_arena/update_clients.py
@@ -5,8 +5,9 @@ from thunderdome.config import game_name
 def update_clients_from_github():
     subs = game_name.split("-")
     repo_name = game_name.split("-")[::-1][0]
-    print repo_name
-
+    for i in get_all_forks(repo_name):
+        print i
+        
 
 if __name__ == "__main__":
     update_clients_from_github()
