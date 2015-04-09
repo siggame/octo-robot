ac74b8c14cd292cbabd725a0f8bde83fbbd301bb
Brandon Phelps <bwp44f@mst.edu>
2015-04-08 23:36:46 -0500

    added a print statement

--- masterblaster/utilities/kmeans.py
+++ masterblaster/utilities/kmeans.py
@@ -147,6 +147,7 @@ def assign_ratings():
         i.save()
     
 def assign_calc_rating(game):
+    print game
     data_point = DataPoint.objects.get(game_id=game.pk)
     assign_cluster(data_point, list(DataPoint.objects.filter(data_point=False)))
     cluster_rating = DataPoint.objects.get(cluster_id=data_point.cluster_id, data_point=False).rating_value
