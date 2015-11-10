69845664acf1e5c8d8a8e18232a2004b4054e9b6
brandon <bwp44f@mst.edu>
2015-11-09 21:40:10 -0600

    clean up

--- masterblaster/utilities/get_test_clients.py
+++ masterblaster/utilities/get_test_clients.py
@@ -12,22 +12,12 @@ def main(fork_list):
         forks_url = requests.get("%s/repos/siggame/%s/forks" % (base_api_uri, fork), headers=header_info)
         data = forks_url.json()
         for i in data:
-            print 
-            print "Adding client", i
-            for j in i.iteritems():
-                print j
-            print 
-            print "login", i["owner"]["login"]
+            print "Adding client", i["owner"]["login"]
             master_hash = requests.get("%s/repos/%s/commits/master" % (base_api_uri, i["full_name"]), headers=header_info)
             test_clients.append(construct_client_block("master", master_hash.json()["sha"], i["ssh_url"],
                                                        i["owner"]["login"], i["language"].lower()))
-
-    print 'Test client'
-    for i in test_clients:
-        print 
-        print i
         
-    # update_clients_from_data_block(test_clients)
+    update_clients_from_data_block(test_clients)
 
 
 def construct_client_block(tag_name, tag_commit, repo_path, team_slug, language):
@@ -39,10 +29,6 @@ def construct_client_block(tag_name, tag_commit, repo_path, team_slug, language)
 
 
 if __name__ == "__main__":
-    fork_list = [("Joueur.py-MegaMinerAI-Dev", "python"), ("Joueur.lua-MegaMinerAI-Dev", "lua"),
-                 ("Joueur.js-MegaMinerAI-Dev", "javascript"), ("Joueur.java-MegaMinerAI-Dev", "java"),
-                 ("Joueur.cpp-MegaMinerAI-Dev", "c++"), ("Joueur.cs-MegaMinerAI-Dev", "csharp")]
-
     fork_list = ["Joueur.py-MegaMinerAI-Dev", "Joueur.lua-MegaMinerAI-Dev", "Joueur.js-MegaMinerAI-Dev",
                  "Joueur.java-MegaMinerAI-Dev", "Joueur.cpp-MegaMinerAI-Dev", "Joueur.cs-MegaMinerAI-Dev"]
     main(fork_list)
