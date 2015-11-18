c8818e58c45da6174d34cf01f87d047f0d381a56
gladiator <gladiator>
2015-11-17 21:44:44 -0600

    added api url for the visualiser

--- arena/urls.py
+++ arena/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     url(r'^mies/thunderdome/', include('arena.thunderdome.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/next_game/$', 'arena.thunderdome.views.get_next_game_url_to_visualize'),
 )
 
 if settings.DEBUG:
