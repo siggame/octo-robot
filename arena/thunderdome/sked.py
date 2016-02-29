509f4611dc44ef90934e725b00a6055934e2826c
Colton Lewis <cwl@protonmail.ch>
2016-02-29 00:54:15 -0600

    This closes #56.
    It gives sked() a static variable to keep track of how many games have
    been scheduled and logs the game number with the rest of the info every
    time sked() is called.

--- arena/thunderdome/sked.py
+++ arena/thunderdome/sked.py
@@ -29,7 +29,9 @@ def sked(guy0, guy1, stalk, origin, priority=1000, ttr=180):
     game.stats = json.dumps(payload)
     stalk.put(game.stats, priority=priority, ttr=ttr)
     game.save()
-    print 'scheduled', game, guy0, payload['clients'][0]['tag'], guy1, payload['clients'][1]['tag']
+    print 'scheduled', sked.game_number, game, guy0, payload['clients'][0]['tag'], guy1, payload['clients'][1]['tag']
+    sked.game_number += 1
     payload.update({'reporter':origin})
     # log.info(json.dumps(payload))
     return game
+sked.game_number = 1
