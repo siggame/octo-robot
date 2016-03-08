f16ca27d164295212562feb894debfbd530c9d20
Colton Lewis <cwl@protonmail.ch>
2016-03-08 02:37:56 -0600

    Revert "This closes #56."
    This reverts commit 509f4611dc44ef90934e725b00a6055934e2826c.
    
    The changes to sked.py were not needed, because it already prints
    game_numbers, though they behave differently from the code I added.

--- arena/thunderdome/sked.py
+++ arena/thunderdome/sked.py
@@ -29,9 +29,7 @@ def sked(guy0, guy1, stalk, origin, priority=1000, ttr=180):
     game.stats = json.dumps(payload)
     stalk.put(game.stats, priority=priority, ttr=ttr)
     game.save()
-    print 'scheduled', sked.game_number, game, guy0, payload['clients'][0]['tag'], guy1, payload['clients'][1]['tag']
-    sked.game_number += 1
+    print 'scheduled', game, guy0, payload['clients'][0]['tag'], guy1, payload['clients'][1]['tag']
     payload.update({'reporter':origin})
     # log.info(json.dumps(payload))
     return game
-sked.game_number = 1
