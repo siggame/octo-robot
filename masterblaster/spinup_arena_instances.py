e24872cc1cdd785d7e9f9cd0f0c3bab46a7078eb
Daniel <dab5ff@mst.edu>
2016-04-07 21:06:31 -0500

    Re-enabled gladiator.py in spinup_arena_instances.py, made improvements There is no reason gladiator.py shouldn't work, and we seem to have solved the issues we thought it was causing. Added ability to pass in number of referees to run on each gladiator and ability to pass in what type of Amazon instance to use.

--- masterblaster/spinup_arena_instances.py
+++ masterblaster/spinup_arena_instances.py
@@ -5,6 +5,22 @@ from thunderdome.config import arena_ami, access_cred, secret_cred, \
 
 import sys
 
+if len(sys.argv) > 1:
+   count = int(sys.argv[1])
+else:
+   count = 1
+   
+if len(sys.argv) > 2:
+   numRefs = int(sys.argv[2])
+else:
+   numRefs = 1
+   
+if len(sys.argv) > 3:
+   instanceType = str(sys.argv[3])
+else:
+   instanceType = 'c3.large'
+
+
 user_data = \
 """#!/bin/bash
 su - gladiator << EOF
@@ -26,31 +42,21 @@ cd /home/gladiator/arena
 wget http://arena.megaminerai.com/gladiator/gladiator_package.tgz
 tar -xf gladiator_package.tgz
 
-#python gladiator.py
-
-mkdir 1
-ln referee.py 1/referee.py
-ln prep_for_bake.py 1/prep_for_bake.py
-cd 1
-python referee.py &
-cd ..
+python gladiator.py '%s'
 
 EOF
-""" % (access_cred, secret_cred, s3_prefix, game_name, client_prefix, beanstalk_host)
+""" % (access_cred, secret_cred, s3_prefix, game_name, client_prefix, beanstalk_host, numRefs)
 
 import boto
 
-if len(sys.argv) > 1:
-   count = int(sys.argv[1])
-else:
-   count = 1
+
 
 print "spinning up %i gladiators..." % count
 conn = boto.connect_ec2(access_cred, secret_cred)
 gladiator_image = conn.get_image(arena_ami)
 reservation = gladiator_image.run(min_count=count, max_count=count,
                                   user_data=user_data,
-                                  instance_type='c3.large',
+                                  instance_type=instanceType,
                                   key_name='mmai-16-glad-key',
                                   security_groups=['Arena Gladiator']) 
 
