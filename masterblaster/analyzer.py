79d4050712d1f457fe28c3ded45b7c3586d48d06
cluster <cluster>
2016-09-23 22:56:57 -0500

    Optimized and added/changed console output in analyser.py

--- masterblaster/analyzer.py
+++ masterblaster/analyzer.py
@@ -60,31 +60,10 @@ def main():
                 x.save()
                 continue
             url = x.gamelog_url
-    
-            file_name = url.split('/')[-1]
             u = urllib2.urlopen(url)
-            f = open(file_name, 'wb')
             meta = u.info()
             file_size = int(meta.getheaders("Content-Length")[0])
-            print "Downloading: %s Bytes: %d" % (file_name, file_size)
-
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                    break
-
-                file_size_dl += len(buffer)
-                f.write(buffer)
-                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-                status = status + chr(8)*(len(status)+1)
-                print status,
-
-            f.close()
-            print ' '
-            os.remove(file_name)
-            print file_size
+            print "Game", x, "is", file_size, "bytes"
             if file_size > gamestats.maxSize:
                 gamestats.maxSize = file_size
             
@@ -104,7 +83,7 @@ def main():
         for x in Game.objects.all().filter(score=-1).filter(status="Complete"):
             x.score = 0
             x.save()
-            print "Now analysing game", x
+            print "\nNow analysing game", x
             analyse_game(x)
         time.sleep(10)
 
@@ -115,30 +94,9 @@ def analyse_game(game):
         game.save()
         return
     url = game.gamelog_url
-
-    file_name = url.split('/')[-1]
     u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
     meta = u.info()
     file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
-
-    f.close()
-    print ' '
-    os.remove(file_name)
     if file_size > gamestats.maxSize:
         gamestats.maxSize = file_size
     gamestats.numPlayed += 1
@@ -157,6 +115,7 @@ def analyse_game(game):
             print "WUMBO!!!!"
             break
     game.save()
+    print "Game score:", game.score
     return
 
 if __name__ == "__main__":
